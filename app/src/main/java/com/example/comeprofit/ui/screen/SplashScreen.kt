6e1fd5850fa3bcc29536de6f6f098fca6672fb0c
Marnyot <marioardhana37@gmail.com>
2025-06-12 13:30:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/comeprofit/ui/screen/SplashScreen.kt
@@ -0,0 +1,77 @@
+package com.example.comeprofit.ui.screen
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.comeprofit.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    onSplashFinished: () -> Unit
+) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 1500),
+        label = "Alpha Animation"
+    )
+
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(2000)
+        onSplashFinished()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.alpha(alphaAnim.value)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_food_placeholder),
+                contentDescription = "Logo",
+                tint = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier.size(100.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "ComeProfit",
+                color = MaterialTheme.colorScheme.onPrimary,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
